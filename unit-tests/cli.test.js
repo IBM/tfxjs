01afd2a3f04ea3f93d994ec06abfa0e7810a6159
tahajafry <tahaabbasjafry@gmail.com>
2022-06-02 11:15:24 -0500

    fixing structure + basic tests + example test json

--- unit-tests/cli.test.js
+++ unit-tests/cli.test.js
@@ -1,8 +1,36 @@
-const { assert } = require("chai");
+const { assert, expect } = require("chai");
+const tfx = require("tfxjs");
 const cli = require("../lib/cli");
+// const mockTfx = require("../lib/cli.js")
+// const tfx = require("../lib/tfx-cli")
+
+// function mockExec(data) {
+//     this.data = data;
+//     this.commandList = [];
+//     this.promise = (command) => {
+//       this.commandList.push(command);
+//       return new Promise((resolve, reject) => {
+//         if (this.data?.stderr) reject(this.data);
+//         else resolve(this.data);
+//       });
+//     };
+//   }
+
+let exec = ''; //doesn't matter, never used
+let spawn = '';//doesn't matter, never used
 
 describe("cli", () => {
    it("infrastructure test", () => {
        assert.equal(true, true);
-   })
+   });
+   it("should run the correct commands with no args ", () => {
+       expect(()=>{cli.cli(cli.mockTfx, exec, spawn)}).to.not.throw();
+   });
+   it("should run the correct commands with one arg", () => {
+    let tfxInst = new cli.mockTfx(exec, spawn, '--help');
+    tfxInst.tfxcli()
+    expect(()=>{cli.cli(cli.mockTfx, exec, spawn)}).to.not.throw(); 
+    assert.deepEqual(tfxInst.commandArgs, ['--help'])
+   });
+
 });
