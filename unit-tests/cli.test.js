3cd6b8aab1035757fb48c59977f8cfcb2bc884fc
Lucas Franke <lfranke@Lucass-MBP.lan>
2022-06-06 10:58:37 -0500

    edited the color tests to be more readable

--- unit-tests/cli.test.js
+++ unit-tests/cli.test.js
@@ -50,7 +50,9 @@ describe("cli", () => {
     assert(argSpy.calledOnceWith(["argument1", "argument2"]))
   });
   it("should run console log with thrown error text", () => {
-    let expectedData = "\u001b[31mthis is an error\u001b[39m";
+    const ansiRed = "\u001b[31m"
+    const ansiDefaultForeground = "\u001b[39m"
+    let expectedData = `${ansiRed}this is an error${ansiDefaultForeground}`;
     cli(mockTfxError, "", "", mockConsole, ["nodepath", "filepath"]);
     assert(mockConsole.log.calledOnceWith("this is an error"));
   });
