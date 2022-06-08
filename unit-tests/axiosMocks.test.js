5421afcf5488e9e1c9d04758eac6b46d9def6b5b
Pseusco <lfranke@vt.edu>
2022-06-08 09:25:45 -0500

    added spies to axiosMocks.test.js, started httpCallback

--- unit-tests/axiosMocks.test.js
+++ unit-tests/axiosMocks.test.js
@@ -1,81 +1,165 @@
 const { assert } = require("chai");
-const { describe, it } = require("mocha");
-const {axiosMain, axiosDot} = require("./axios.mocks");
+const { axiosMain, axiosDot } = require("./axios.mocks");
+const sinon = require("sinon");
 
-const optionTracker = function () {
-    this.set = function (value) {
-        this.value = value;
-    };
-    this.lastCall = () => {
-        return this.value;
-    };
-};
+let aDotReject, aDotResolve;
 
 describe("axiosMocks", () => {
-    describe("axiosMain", () => {
-        it("should return a Promise that resolves when no err", () => {
-            let aMain = axiosMain(new optionTracker, "test", false);
-            return aMain('testing', 'test').then( (data) => {
-                assert.deepEqual(data, {data: "test"});
-            });
-        });
-        it("should return a Promise that rejects when err is caught", () => {
-            let aMain = axiosMain(new optionTracker, "test" , {stderr: "Gotcha! Promise rejected!"});
-            return aMain('testing', 'test').catch((data) => {
-                assert.deepEqual(data, {stderr: "Gotcha! Promise rejected!"}, "should return");
-            })
-        }); 
+  describe("axiosMain", () => {
+    it("should return a Promise that resolves when no err", () => {
+      let aMain = axiosMain("test", false);
+      return aMain("testing", "test").then((data) => {
+        assert.deepEqual(data, { data: "test" });
+      });
     });
-    describe("axiosDot", () => {
-        let aDotReject = axiosDot(new optionTracker, "test", {stderr: "Gotcha! Promise rejected!"});
-        let aDotResolve = axiosDot(new optionTracker, "test", false);
-        describe("get", () => {
-            it("should return the correct data when err is false", () => {
-                return aDotResolve.get('', '').then((data) => {
-                    assert.deepEqual(data, {data: "test"});
-                })
-            });
-            it("should be rejected when err is caught", () => {
-                return aDotReject.get('', '').catch((data) => {
-                    assert.deepEqual(data, {stderr: "Gotcha! Promise rejected!"});
-                })
-            });
-        });
-        describe("post", () => {
-            it("should return the correct data when err is false", () => {
-                return aDotResolve.post('', '', '').then((data) => {
-                    assert.deepEqual(data, "success");
-                })
-            });
-            it("should be rejected when err is caught", () => {
-                return aDotReject.post('', '', '').catch((data) => {
-                    assert.deepEqual(data, {stderr: "Gotcha! Promise rejected!"});
-                })
-            });
-        });
-        describe("delete", () => {
-            it("should return the correct data when err is false", () => {
-                return aDotResolve.delete('', '').then((data) => {
-                    assert.deepEqual(data, "success");
-                })
-            });
-            it("should be rejected when err is caught", () => {
-                return aDotReject.delete('', '').catch((data) => {
-                    assert.deepEqual(data, {stderr: "Gotcha! Promise rejected!"});
-                })
-            });
-        });
-        describe("put", () => {
-            it("should return the correct data when err is false", () => {
-                return aDotResolve.put('', '', '').then((data) => {
-                    assert.deepEqual(data, "success");
-                })
-            });
-            it("should be rejected when err is caught", () => {
-                return aDotReject.put('', '', '').catch((data) => {
-                    assert.deepEqual(data, {stderr: "Gotcha! Promise rejected!"});
-                })
-            });
-        });
+    it("should return a Promise that rejects when err is caught", () => {
+      let aMain = axiosMain("test", { stderr: "Gotcha! Promise rejected!" });
+      return aMain("testing", "test").catch((data) => {
+        assert.deepEqual(
+          data,
+          { stderr: "Gotcha! Promise rejected!" },
+          "should return"
+        );
+      });
     });
+  });
+  describe("axiosDot", () => {
+    beforeEach(() => {
+      aDotReject = axiosDot("test", { stderr: "Gotcha! Promise rejected!" });
+      aDotResolve = axiosDot("test", false);
+
+      aDotReject.get = sinon.spy(aDotReject, "get");
+      aDotResolve.get = sinon.spy(aDotResolve, "get");
+
+      aDotReject.post = sinon.spy(aDotReject, "post");
+      aDotResolve.post = sinon.spy(aDotResolve, "post");
+
+      aDotReject.delete = sinon.spy(aDotReject, "delete");
+      aDotResolve.delete = sinon.spy(aDotResolve, "delete");
+
+      aDotReject.put = sinon.spy(aDotReject, "put");
+      aDotResolve.put = sinon.spy(aDotResolve, "put");
+    });
+
+    describe("get", () => {
+      it("should return the correct data when err is false", () => {
+        return aDotResolve
+          .get("the url", "the options")
+          .then((data) => {
+            assert.deepEqual(data, { data: "test" });
+          })
+          .finally(() => {
+            assert.isTrue(
+              aDotResolve.get.calledOnceWith("the url", "the options")
+            );
+          });
+      });
+      it("should be rejected when err is caught", () => {
+        return aDotReject
+          .get("the url", "the options")
+          .catch((data) => {
+            assert.deepEqual(data, { stderr: "Gotcha! Promise rejected!" });
+          })
+          .finally(() => {
+            assert.isTrue(
+              aDotReject.get.calledOnceWith("the url", "the options")
+            );
+          });
+      });
+    });
+    describe("post", () => {
+      it("should return the correct data when err is false", () => {
+        return aDotResolve
+          .post("the url", "the body", "the options")
+          .then((data) => {
+            assert.deepEqual(data, "success");
+          })
+          .finally(() => {
+            assert.isTrue(
+              aDotResolve.post.calledOnceWith(
+                "the url",
+                "the body",
+                "the options"
+              )
+            );
+          });
+      });
+      it("should be rejected when err is caught", () => {
+        return aDotReject
+          .post("the url", "the body", "the options")
+          .catch((data) => {
+            assert.deepEqual(data, { stderr: "Gotcha! Promise rejected!" });
+          })
+          .finally(() => {
+            assert.isTrue(
+              aDotReject.post.calledOnceWith(
+                "the url",
+                "the body",
+                "the options"
+              )
+            );
+          });
+      });
+    });
+    describe("delete", () => {
+      it("should return the correct data when err is false", () => {
+        return aDotResolve
+          .delete("the url", "the options")
+          .then((data) => {
+            assert.deepEqual(data, "success");
+          })
+          .finally(() => {
+            assert.isTrue(
+              aDotResolve.delete.calledOnceWith("the url", "the options")
+            );
+          });
+      });
+      it("should be rejected when err is caught", () => {
+        return aDotReject
+          .delete("the url", "the options")
+          .catch((data) => {
+            assert.deepEqual(data, { stderr: "Gotcha! Promise rejected!" });
+          })
+          .finally(() => {
+            assert.isTrue(
+              aDotReject.delete.calledOnceWith("the url", "the options")
+            );
+          });
+      });
+    });
+    describe("put", () => {
+      it("should return the correct data when err is false", () => {
+        return aDotResolve
+          .put("the url", "the body", "the options")
+          .then((data) => {
+            assert.deepEqual(data, "success");
+          })
+          .finally(() => {
+            assert.isTrue(
+              aDotResolve.put.calledOnceWith(
+                "the url",
+                "the body",
+                "the options"
+              )
+            );
+          });
+      });
+      it("should be rejected when err is caught", () => {
+        return aDotReject
+          .put("the url", "the body", "the options")
+          .catch((data) => {
+            assert.deepEqual(data, { stderr: "Gotcha! Promise rejected!" });
+          })
+          .finally(() => {
+            assert.isTrue(
+              aDotReject.put.calledOnceWith(
+                "the url",
+                "the body",
+                "the options"
+              )
+            );
+          });
+      });
+    });
+  });
 });
