c2a0115c5e63769f3e6e060f140eb727c8aa9618
Taha Jafry <Taha.Jafry@ibm.com>
2022-06-09 15:10:12 -0500

    quiet tests in extract and getting 100% code cov

--- unit-tests/tfx-cli.test.js
+++ unit-tests/tfx-cli.test.js
@@ -1,7 +1,8 @@
 const { assert } = require("chai");
 const cli = require("../lib/tfx-cli");
-const chalk = require("chalk");
 const constants = require("../lib/constants");
+const sinon = require("sinon");
+const { isFunction } = require("../lib/helpers");
 
 function mockExec(data) {
   this.data = data;
@@ -54,15 +55,6 @@ describe("cli", () => {
       };
       tfWithLogs.print("frog");
       assert.deepEqual(actualData, "frog", "it should return exact data");
-    });
-    it("should not print with quiet flag", () => {
-      let tfWithLogs = new cli(exec.promise, spawn.promise, "--quiet");
-      let actualData;
-      tfWithLogs.log = (data) => {
-        actualData = data;
-      };
-      tfWithLogs.print("frog");
-      assert.deepEqual(actualData, undefined, "it should not return anything");
     });
   });
   describe("tfxcli", () => {
@@ -124,10 +116,15 @@ describe("cli", () => {
     });
   });
   describe("plan", () => {
+    let planTfxSpy, writeFileCallBackSpy;
+    beforeEach(() => {
+      planTfxSpy = new sinon.spy();
+      writeFileCallBackSpy = new sinon.spy();
+    });
     it("should run extract with correct commands and flags for plan and write data", () => {
       tfx = new cli(
-        exec.promise,
-        spawn.promise,
+        "exec.promise",
+        "spawn.promise",
         "plan",
         "--in",
         "./filePath",
@@ -142,47 +139,30 @@ describe("cli", () => {
         "-v",
         "testValue3=3"
       );
-      let actualData = [];
-      let expectedData = [
-        "tfx Generated Plan",
-        "./filePath",
-        "tfx",
-        {
-          testVar1: true,
-          testVar2: "true",
-          testValue3: 3,
-        },
-        false,
-      ];
+      tfx.planTfx = planTfxSpy;
 
-      tfx.planTfx = (...args) => {
-        let callback = args.pop(); // remove callback
-        let shallow = args.pop(); // remove child
-        args.pop();
-        actualData = args.concat([shallow]);
-        callback("fileData");
-      };
-      let actualCallback = [];
-      let expectedCallback = ["./out-file-path", "fileData"];
-      tfx.writeFileSync = (filePath, data) => {
-        actualCallback = [filePath, data];
-      };
+      tfx.writeFileCallBack = writeFileCallBackSpy;
+
       tfx.tfxcli();
-      assert.deepEqual(
-        actualData,
-        expectedData,
+      assert.isTrue(
+        planTfxSpy.calledOnceWith(
+          "tfx Generated Plan",
+          "./filePath",
+          "tfx",
+          { testVar1: true, testVar2: "true", testValue3: 3 },
+          "exec.promise",
+          false,
+          false
+        ),
         "it should return correct params"
       );
-      assert.deepEqual(
-        actualCallback,
-        expectedCallback,
-        "it should run writeFileSync with correct params"
-      );
+
+      assert.isTrue(writeFileCallBackSpy.calledOnceWith("./out-file-path"));
     });
     it("should run extract with correct commands and flags for plan and write data when shallow", () => {
       tfx = new cli(
-        exec.promise,
-        spawn.promise,
+        "exec.promise",
+        "spawn.promise",
         "plan",
         "--in",
         "./filePath",
@@ -198,42 +178,65 @@ describe("cli", () => {
         "-v",
         "testValue3=3"
       );
-      let actualData = [];
-      let expectedData = [
-        "tfx Generated Plan",
-        "./filePath",
-        "tfx",
-        {
-          testVar1: true,
-          testVar2: "true",
-          testValue3: 3,
-        },
-        true,
-      ];
+      tfx.planTfx = planTfxSpy;
 
-      tfx.planTfx = (...args) => {
-        let callback = args.pop(); // remove callback
-        let shallow = args.pop(); // remove child
-        args.pop();
-        actualData = args.concat([shallow]);
-        callback("fileData");
-      };
-      let actualCallback = [];
-      let expectedCallback = ["./out-file-path", "fileData"];
-      tfx.writeFileSync = (filePath, data) => {
-        actualCallback = [filePath, data];
-      };
+      tfx.writeFileCallBack = writeFileCallBackSpy;
+
       tfx.tfxcli();
-      assert.deepEqual(
-        actualData,
-        expectedData,
+      assert.isTrue(
+        planTfxSpy.calledOnceWith(
+          "tfx Generated Plan",
+          "./filePath",
+          "tfx",
+          { testVar1: true, testVar2: "true", testValue3: 3 },
+          "exec.promise",
+          true,
+          false
+        ),
         "it should return correct params"
       );
-      assert.deepEqual(
-        actualCallback,
-        expectedCallback,
-        "it should run writeFileSync with correct params"
+
+      assert.isTrue(writeFileCallBackSpy.calledOnceWith("./out-file-path"));
+    });
+    it("should run extract with correct commands and flags for plan and write data when quiet", () => {
+      tfx = new cli(
+        "exec.promise",
+        "spawn.promise",
+        "plan",
+        "--in",
+        "./filePath",
+        "--out",
+        "./out-file-path",
+        "-q",
+        "--type",
+        "tfx",
+        "-v",
+        "testVar1=true",
+        "-v",
+        'testVar2="true"',
+        "-v",
+        "testValue3=3"
       );
+
+      tfx.planTfx = planTfxSpy;
+
+      tfx.writeFileCallBack = writeFileCallBackSpy;
+
+      tfx.tfxcli();
+      assert.isTrue(
+        planTfxSpy.calledOnceWith(
+          "tfx Generated Plan",
+          "./filePath",
+          "tfx",
+          { testVar1: true, testVar2: "true", testValue3: 3 },
+          "exec.promise",
+          false,
+          true
+        ),
+        "it should return correct params"
+      );
+
+      assert.isTrue(writeFileCallBackSpy.calledOnceWith("./out-file-path"));
     });
   });
   describe("decode", () => {
@@ -286,6 +289,22 @@ describe("cli", () => {
         expectedFileData,
         "it should return correct file data"
       );
+    });
+  });
+  describe("writeFileCallBack", () => {
+    tfx = new cli();
+    let writeFileSyncSpy;
+    beforeEach(() => {
+      writeFileSyncSpy = sinon.spy();
+      tfx.writeFileSync = writeFileSyncSpy;
+    });
+    it("should return a function", () => {
+      assert.isFunction(tfx.writeFileCallBack());
+    });
+    it("should return a function that calls writeFileSync with the correct parameters", () => {
+      let callbackFunction = tfx.writeFileCallBack("outFilePath");
+      callbackFunction("filedata");
+      assert(writeFileSyncSpy.calledOnceWith("outFilePath", "filedata"));
     });
   });
 });
