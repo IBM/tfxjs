0c92842f08e1f8a6c8dff682a3cd77d05cff4311
Taha Jafry <Taha.Jafry@ibm.com>
2022-07-25 13:35:45 -0500

    fixed ssh mock

--- unit-tests/connect.test.js
+++ unit-tests/connect.test.js
@@ -1,3 +1,7 @@
+const { assert } = require("chai");
+const connect = require("../lib/connect");
+const node_ssh = require("node-ssh");
+const ping = require("ping");
 let mockSshPackage = {
   connect: (data) => {
     if (data) {
@@ -11,3 +15,43 @@ let mockSshPackage = {
     }
   },
 };
+
+let mockPingPackage = {
+  promise: {
+    probe: (host) => {
+      if (host) {
+        return new Promise((resolve, reject) => {
+          resolve(host);
+        });
+      } else {
+        return new Promise((resolve, reject) => {
+          reject("connection failure");
+        });
+      }
+    },
+  },
+};
+
+describe("SSH Tests", function () {
+  it("should connect with mock ssh", () => {
+    return connect.sshTest(mockSshPackage, "host", "name", "key", false);
+  }),
+    it("should connect with correct credentials real ssh", () => {
+      return connect.sshTest(
+        new node_ssh.NodeSSH(),
+        "150.239.84.248",
+        "root",
+        "/Users/tahajafry/.ssh/id_rsa_ibm",
+        false
+      );
+    });
+});
+
+describe("Ping Tests", function () {
+  it("should connect with mock ping", () => {
+    return connect.pingTest(mockPingPackage, "host", false);
+  }),
+    it("should connect with ping", () => {
+      return connect.pingTest(ping, "host", false);
+    });
+});
