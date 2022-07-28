fb0d271d26fb48f56a1af16577ca88e63082ea0d
tahajafry <tahaabbasjafry@gmail.com>
2022-07-28 14:11:53 -0500

    moved mocks

--- unit-tests/connect.test.js
+++ unit-tests/connect.test.js
@@ -1,40 +1,15 @@
 const { assert } = require("chai");
 const connect = require("../lib/connect");
-let mockSshPackage = function (err) {
-  this.connected = false;
-  this.isConnected = function () {
-    return this.connected;
-  };
-  this.connect = function (data) {
-    return new Promise((resolve, reject) => {
-      if (err) {
-        this.connected = false;
-        reject("connection failure");
-      } else {
-        this.connected = true;
-        resolve();
-      }
-    });
-  };
-};
-
-let mockPingPackage = function (err) {
-  this.promise = {
-    probe: function (host) {
-      return new Promise((resolve, reject) => {
-        if (err) {
-          resolve({ alive: false });
-        } else {
-          resolve({ alive: true });
-        }
-      });
-    },
-  };
-};
+const mocks = require("./tfx.mocks");
+let mock = new mocks();
+let mockSshPackage = new mock.mockSshPackage();
+let errMockSshPackage = new mock.mockSshPackage(true);
+let mockPingPackage = new mock.mockPingPackage();
+let errMockPingPackge = new mock.mockPingPackage(true);
 
 describe("SSH Tests", function () {
   it("should connect with mock ssh", () => {
-    let sshConn = new connect({ ssh: new mockSshPackage()});
+    let sshConn = new connect({ ssh: mockSshPackage });
     return sshConn.sshTest(
       "passing connecting ssh test",
       "host",
@@ -43,7 +18,7 @@ describe("SSH Tests", function () {
     );
   });
   it("should not connect if doesNotConnect is true", () => {
-    let sshConn = new connect({ ssh: new mockSshPackage(true)});
+    let sshConn = new connect({ ssh: errMockSshPackage });
     return sshConn.sshTest(
       "passing connecting ssh test",
       "host",
@@ -53,7 +28,7 @@ describe("SSH Tests", function () {
     );
   });
   it("should throw an error if it doesn't connect when it should", () => {
-    let sshConn = new connect({ ssh: new mockSshPackage(true) });
+    let sshConn = new connect({ ssh: errMockSshPackage });
     return sshConn
       .sshTest("failing not connecting ssh test", "host", "name", "key")
       .catch((err) => {
@@ -64,7 +39,7 @@ describe("SSH Tests", function () {
       });
   });
   it("should throw an error when it connects and its not supposed to", () => {
-    let sshConn = new connect({ ssh: new mockSshPackage() });
+    let sshConn = new connect({ ssh: mockSshPackage });
     return sshConn
       .sshTest("failing connecting ssh test", "host", "name", "key", true)
       .catch((err) => {
@@ -78,15 +53,15 @@ describe("SSH Tests", function () {
 
 describe("Ping Tests", function () {
   it("should connect with mock ping", () => {
-    let pingConn = new connect({ ping: new mockPingPackage() });
+    let pingConn = new connect({ ping: mockPingPackage });
     return pingConn.pingTest("passing connecting ping test", "host");
   });
   it("should not connect with doesNotConnect = true", () => {
-    let pingConn = new connect({ ping: new mockPingPackage(true) });
+    let pingConn = new connect({ ping: errMockPingPackge });
     return pingConn.pingTest("passing not connecting ping test", "host", true);
   });
   it("should throw an error when connects and doesNotConnect = true", () => {
-    let pingConn = new connect({ ping: new mockPingPackage() });
+    let pingConn = new connect({ ping: mockPingPackage });
     return pingConn
       .pingTest("failing connecting ping test", "host", true)
       .catch((err) => {
@@ -97,8 +72,8 @@ describe("Ping Tests", function () {
         );
       });
   });
-  it("should throw an error when doesn't and doesNotConnect = false", () => {
-    let pingConn = new connect({ ping: new mockPingPackage(true) });
+  it("should throw an error when doesn't connect and doesNotConnect = false", () => {
+    let pingConn = new connect({ ping: errMockPingPackge });
     return pingConn
       .pingTest("failing not connecting ping test", "host")
       .catch((err) => {
