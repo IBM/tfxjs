25490a7cf770dbff71ccc51ac76cd4720f96ef59
waltertang <waltertang@ibm.com>
2022-07-28 12:45:45 -0700

    initial test

--- unit-tests/connect.test.js
+++ unit-tests/connect.test.js
@@ -1,31 +1,19 @@
 const { assert } = require("chai");
 const connect = require("../lib/connect");
+const mocks = require('./tfx.mocks')
 
 let mockLib = new mocks();
 
-let mockTcpPackage = function (err) {
-  promise: {
-    tcp_connect: (data) => {
-      if (data) {
-        return new Promise((resolve, reject) => {
-            resolve(host);
-        })
-      }
-      else {
-        return new Promise((resolve, reject) => {
-            reject("Connection failure");
-        })
-      }
-    };
-  }
-};
+
 
 describe("Testing the TCP connection", () => {
   it("should successfully connect to the server using the port", () => {
-    let connectPackage = new connect({exec: mockLib.})
-    return connect.tcpTest("host", "port", false);
+    let connectPackage = new connect({exec: mockLib.tcpExec(true)});
+    return connectPackage.tcpTest("host", "port", false);
+    //return connect.tcpTest("host", "port", false);
   });
   it("should not connect when the port does not exist", () => {
-    return connect.tcpTest("host", "port", true)
+    let connectPackage = new connect({exec: mockLib.tcpExec(false)});
+    return connectPackage.tcpTest("host", "port", true);
   })
 });
