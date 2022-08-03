d6af6177aef5fc786507dc3cb750f44eb308dd64
Pseusco <lfranke@vt.edu>
2022-08-03 10:53:40 -0500

    modified the existing mock exec function in tfx.mocks to work with UDP tests

--- unit-tests/connect.test.js
+++ unit-tests/connect.test.js
@@ -1,15 +1,20 @@
-const mocks = require('./tfx.mocks');
+const mocks = require("./tfx.mocks");
 const connect = require("../lib/connect");
 
 let mockLib = new mocks();
 
 describe("UDP connection", () => {
   it("should run a passing test when an expected UDP connection is successful", () => {
-    let connectPackage = new connect({exec: mockLib.udpExec(true)})
-    return connectPackage.udpTest("host", "port", {timeout: 1000}, false);
+    let mockExec = new mockLib.mockExec({ stdout: "", stderr: "" }, true);
+    let connectPackage = new connect({ exec: mockExec.promise });
+    return connectPackage.udpTest("host", "port", 1000, false);
   });
   it("should run a passing test when an expected unsuccessful UDP connection is refused", () => {
-    let connectPackage = new connect({exec: mockLib.udpExec(false)})
-    return connectPackage.udpTest("host", "port", {timeout: 1000}, true);
+    let mockExec = new mockLib.mockExec(
+      { stdout: "", stderr: "read(net): Connection refused\n" },
+      false
+    );
+    let connectPackage = new connect({ exec: mockExec.promise });
+    return connectPackage.udpTest("host", "port", 1000, true);
   });
 });
