8a552b9d675eba43cf8c8b7dab42f77e62829fbe
Lucas Franke <Lucas.Franke@ibm.com>
2022-08-03 10:53:40 -0500

    modified the existing mock exec function in tfx.mocks to work with UDP tests

--- unit-tests/tfx.mocks.js
+++ unit-tests/tfx.mocks.js
@@ -68,51 +68,24 @@ const mocks = function () {
   /**
    * Create a mock exec function
    * @param {Object} data arbitrary data bject to return
+   * @param {boolean} shouldTimeout determines whether the function should timeout or not
    */
-  this.mockExec = function (data) {
+  this.mockExec = function (data, shouldTimeout) {
     this.data = data;
+    this.shouldTimeout = shouldTimeout;
     this.commandList = [];
-    this.promise = (command) => {
+    this.promise = (command, options) => {
       this.commandList.push(command);
       return new Promise((resolve, reject) => {
-        if (this.data?.stderr) reject(this.data);
+        if (this.shouldTimeout) {
+          setTimeout(() => {
+            reject(this.data);
+          }, options.timeout);
+        } else if (this.data?.stderr) reject(this.data);
         else resolve(this.data);
       });
     };
   };
-
-  /**
-   * Create a mock exec function for a UDP connection using GNU netcat version 0.7.1.
-   * When successful, the function will time out with no output.
-   * Upon failure, the Promise will reject with a message stored in stderr.
-   * @param {boolean} success determines whether the mock function should succeed or fail
-   * @returns mock exec function for making a UDP connection to a server
-   */
-  this.udpExec = function (success) {
-      return function (
-        cmd,
-        options = {
-          cwd: process.cwd(),
-          env: process.env,
-          encoding: "utf8",
-          shell: "/bin/sh",
-          timeout: 0,
-          maxBuffer: 1024 * 1024,
-          killSignal: "SIGTERM",
-          windowsHide: false,
-        }
-      ) {
-        return new Promise((resolve, reject) => {
-          if (success) {
-            setTimeout(() => {
-              reject({ stdout: "", stderr: "" });
-            }, options.timeout);
-          } else {
-            reject({ stdout: "", stderr: "read(net): Connection refused\n" });
-          }
-        });
-      };
-    };
 };
 
 module.exports = mocks;
