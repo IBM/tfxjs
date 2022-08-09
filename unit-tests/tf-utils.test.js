720fe50c9834c6601da401ba83ce8bb1fbbad025
Lucas Franke <Lucas.Franke@ibm.com>
2022-08-09 12:18:24 -0500

    editing tf-utils to work with connect udp functions, incomplete

--- unit-tests/tf-utils.test.js
+++ unit-tests/tf-utils.test.js
@@ -6,6 +6,13 @@ const {
 } = require("../lib/builders.js");
 const tfUnitTestUtils = require("../lib/tf-utils.js");
 const sinon = require("sinon");
+const mocks = require("./tfx.mocks");
+const tfxjs = require("../lib/index");
+const connect = require("../lib/connect");
+let tfx = new tfxjs("./mock_path");
+let mock = new mocks();
+let mockUdpPackage = new mock.mockExec({ stdout: "", stderr: "" }, true).promise;
+let errMockUdpPackage = new mock.mockExec({ stdout: "", stderr: "read(net): Connection refused\n" }, false).promise;
 
 //Initialize spies
 let describeSpy = new sinon.spy();
@@ -22,6 +29,7 @@ const tfutils = new tfUnitTestUtils({
     return callback();
   },
   overrideAssert: assert,
+  overrideExec: mockUdpPackage
 });
 
 describe("tfUnitTestUtils", () => {
@@ -445,6 +453,70 @@ describe("tfUnitTestUtils", () => {
     });
   });
   describe("buildInstanceTest", () => {
+    it("should return the correct test for a function for udp test", () => {
+      let actualData = tfutils.buildInstanceTest(
+        "module.vpc.ibm_is_floating_ip.floating_ip",
+        {
+          resources: [
+            {
+              module: "module.vpc",
+              mode: "managed",
+              type: "ibm_is_floating_ip",
+              name: "floating_ip",
+              instances: [
+                {
+                  attributes: {
+                    address: "host"
+                  }
+                }
+              ]
+            }
+          ]
+        },
+        {
+          0: {
+            address: tfutils.connect.connectionTest(address => {
+              tfutils.connect.udp.doesConnect(address, 8080)
+            })
+          }
+        }
+      )
+      let expectedData = {
+        describe: "module.vpc.ibm_is_floating_ip.floating_ip",
+        tests: [
+          notFalseTest(
+            "Resource module.vpc.ibm_is_floating_ip.floating_ip should be in tfstate",
+            [
+              true,
+              "Expected module.vpc.ibm_is_floating_ip.floating_ip resource to be included in tfstate",
+            ]
+          ),
+          // describe("udpTest", () => {
+          //   it("should connect on port 8080 with UDP", () => {return new connect({ exec: mockUdpPackage }).udpTest("host", 8080, false, 1)})
+          // }),
+          isTrueTest(
+            "Expected instance with key 0 to exist at module.vpc.ibm_is_floating_ip.floating_ip",
+            [
+              true,
+              "Expected instance with key 0 to exist at module.vpc.ibm_is_floating_ip.floating_ip.instances",
+            ]
+          ),
+        ],
+        connectionTests: [
+          {
+            name: "addressTest",
+            arg: "host",
+            fn: tfutils.connect.connectionTest(address => {
+              tfutils.connect.udp.doesConnect(address, 8080)
+            })
+
+          }
+        ]
+
+      };
+      console.log(actualData);
+      assert.deepEqual(actualData, expectedData, "should return correct data");
+    })
     it("should return the correct test for a function for unfound attribute of instance at index 0", () => {
       let actualData = tfutils.buildInstanceTest(
         "module.landing_zone.ibm_atracker_target.atracker_target",
@@ -1068,6 +1140,102 @@ describe("tfUnitTestUtils", () => {
             [
               "module.landing_zone.data.ibm_container_cluster_versions.cluster_versions",
             ],
+          ],
+          "should return correct it function were run"
+        );
+      });
+      it("should run the correct describe and test function for apply with connection tests", () => {
+        let tfstate = {
+          resources: [
+            {
+              module: "module.landing_zone",
+              mode: "data",
+              type: "ibm_container_cluster_versions",
+              name: "cluster_versions",
+              instances: [
+                {
+                  index_key: 0,
+                  attributes: {
+                    name: "name-one",
+                  },
+                },
+                {
+                  index_key: "test",
+                  attributes: {
+                    name: "name-two",
+                  },
+                },
+              ],
+            },
+            {
+              module: "module.landing_zone",
+              mode: "data",
+              type: "ibm_resource_instance",
+              name: "cos",
+              provider: 'provider["registry.terraform.io/ibm-cloud/ibm"]',
+              instances: [],
+            },
+            {
+              module: "module.landing_zone",
+              mode: "data",
+              type: "test",
+              name: "test",
+              instances: [
+                {
+                  index_key: 0,
+                  attributes: {
+                    address: "1.2.3.4"
+                  }
+                }
+              ]
+            }
+          ],
+        };
+        let options = {
+          moduleName: "Connection Test",
+          address:
+            "module.landing_zone.data.test.test",
+          tfData: tfstate,
+          isApply: true,
+          testList: [
+            {
+              name: "Connection Test",
+              address:
+                "module.landing_zone.data.test.test",
+              instances: [
+                {
+                  address: tfutils.connect.connectionTest(address => {
+                    tfutils.connect.udp.doesConnect(address, 8080)
+                  })
+                },
+              ],
+            },
+          ],
+        };
+        tfutils.testModule(options);
+        assert.deepEqual(
+          itSpy.args,
+          [
+            ["addressTest"],
+            [
+              "Resource module.landing_zone.data.test.test should be in tfstate"
+            ],
+            [
+              "Expected instance with key 0 to exist at module.landing_zone.data.test.test"
+            ]
+          ],
+          "should return correct it function were run"
+        );
+        assert.deepEqual(
+          describeSpy.args,
+          [
+            ["Connection Test"],
+            [
+              "module.landing_zone.data.test.test connection tests",
+            ],
+            [
+              "module.landing_zone.data.test.test"
+            ]
           ],
           "should return correct it function were run"
         );
